8fc5994a18ddd00801644e97b49e15bae556a5b0
wangsungtaek <tjdxorv@naver.com>
2024-11-16 21:19:25 +0900

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Tech Mentor</div>
+    </>
+  );
+}
+
+export default App;
